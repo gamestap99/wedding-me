8502eb7e142bd32d5a2d7a1a5c3fa9ddd25249ef
Đinh Văn Nam <itchange.nw@gmail.com>
2024-11-08 02:16:36 +0700

    Update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,10 +63,21 @@ const Home = () => {
     const _buildSuccess = () => {
 
         return (
-            <div className={"flex flex-col p-2 items-center text-3xl text-center"}>
+            <div className={"flex flex-col p-2 items-center text-3xl text-center px-4"}>
                 <p className={`${svnPRIMARK.className}`}>
                     Cảm ơn Bạn <br/>đã dành thời gian xác nhận tham dự <br/>của chúng tớ!
                 </p>
+                <div className={"flex justify-center pt-3"}>
+                    <Typography.Link href="https://quangminhbazi.my.canva.site/saveourdate/"
+                                     className={"flex flex-row justify-center items-center"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                            <path fillRule="evenodd"
+                                  d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"></path>
+                        </svg>
+                        &nbsp;<span className={"underline"}>Về website Đám cưới</span>
+                    </Typography.Link>
+                </div>
             </div>
         )
     }
@@ -223,7 +234,6 @@ const Home = () => {
                             </Form.Item>
                         </div>
                         <div className={"flex justify-center"}>
-
                             <Typography.Link href="https://quangminhbazi.my.canva.site/saveourdate/"
                                              className={"flex flex-row justify-center items-center"}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
